57f8920ff90fe2ccd4825855107c57ae1ffd085d
Javier Galarza <jegj57@gmail.com>
2022-08-03 20:28:12 -0500

    Add an centralized error handler and a builtin error

--- api/index.js
+++ api/index.js
@@ -4,25 +4,12 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-// const mapRoutes = require('express-routes-mapper');
 
-// Load routers here
+const errorHandler = require('../lib/errorHandler');
+// Routers
 const todosRouter = require('./todos/router');
 
-/**
- * server configuration
- */
-// const config = require('../config');
-// const dbService = require('./services/db.service');
-// const auth = require('../auth.policy');
-
-// environment: development, staging, testing, production
-// const environment = process.env.NODE_ENV;
-
 const app = express();
-// const mappedOpenRoutes = mapRoutes(config.publicRoutes, 'api/controllers/');
-// const mappedAuthRoutes = mapRoutes(config.privateRoutes, 'api/controllers/');
-// const DB = dbService(environment, config.migrate).start();
 
 // allow cross origin requests
 // configure to only allow requests from certain origins
@@ -32,24 +19,20 @@ app.use(cors());
 app.use(helmet({
   dnsPrefetchControl: false,
   frameguard: false,
-  ieNoOpen: false,
+  ieNoOpen: false
 }));
 
 // parsing the request bodys
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/todos', todosRouter);
-
-app.get('/', (req, res) => {
-  res.send('hello world');
+// Error handling middleware, we delegate the handling to the centralized error handler
+app.use(async (err, _req, res) => {
+  await errorHandler.handleError(err, res);
 });
 
 // secure your private routes with jwt authentication middleware
 // app.all('/private/*', (req, res, next) => auth(req, res, next));
-
-// // fill routes for express application
-// app.use('/public', mappedOpenRoutes);
-// app.use('/private', mappedAuthRoutes);
+app.use('/todos', todosRouter);
 
 module.exports = app;
